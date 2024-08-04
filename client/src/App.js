30f84b1db3ca117c528c7ff396591b3a0b7eeb5c
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2024-08-03 21:49:09 -0400

    fixed bugs in webcrawling api, added charts

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,81 @@
-// client/src/App.js
-
 import React from "react";
-import logo from "./logo.svg";
+import { ResponsiveContainer, CartesianGrid, XAxis, YAxis, Tooltip, Legend, LineChart, Line } from 'recharts';
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState([]);
-
+  const [courses, setCourses] = React.useState([]);
+  const [chartData1, setChartData1] = React.useState('');
+  const [chartData2, setChartData2] = React.useState('');
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => {
-        setData(data.courseCodes);
-        console.log(data.sections);
+        setCourses(data.courses.map((course => [course.subject, course.code, course.title])));
+        setChartData1(JSON.stringify(data.chartData1));
+        setChartData2(JSON.stringify(data.chartData2));
       });
   }, []);
+
+  const table1 = courses.map(course =>
+    <tr>
+      <td>{`${course[0]} ${course[1]}`}</td>
+      <td>{course[2]}</td>
+    </tr>
+  );
+
+  let chart1 = null;
+  if(chartData1 != '') {
+    chart1 = <ResponsiveContainer width="80%" height={500}>
+      <LineChart data={JSON.parse(chartData1)}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="Monday" stroke="blue" />
+        <Line type="monotone" dataKey="Tuesday" stroke="red" />
+        <Line type="monotone" dataKey="Wednesday" stroke="DarkOrange" />
+        <Line type="monotone" dataKey="Thursday" stroke="green" />
+        <Line type="monotone" dataKey="Friday" stroke="purple" />
+      </LineChart>
+    </ResponsiveContainer>
+  }
+
+  let chart2 = null;
+  if(chartData2 != '') {
+    chart2 = <ResponsiveContainer width="80%" height={500}>
+      <LineChart data={JSON.parse(chartData2)}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="Monday" stroke="blue" />
+        <Line type="monotone" dataKey="Tuesday" stroke="red" />
+        <Line type="monotone" dataKey="Wednesday" stroke="DarkOrange" />
+        <Line type="monotone" dataKey="Thursday" stroke="green" />
+        <Line type="monotone" dataKey="Friday" stroke="purple" />
+      </LineChart>
+    </ResponsiveContainer>
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <div height={100}></div><br></br>
+        {chart1}
+        {chart2}
+        <table>
+          <tbody>
+            {table1}
+          </tbody>
+        </table>
       </header>
     </div>
   );
 }
+
 
 export default App;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const cheerio = require("cheerio"); 
+const cheerio = require("cheerio");
 const app = express();
 const port = 3000;
 
@@ -12,58 +12,150 @@ app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
 
-const daysOfWeekAbbre = ['M', 'T', 'W', 'Th', 'F', 'S', 'U'];
+const staticData = require('./data.json');
+const subjects = staticData.subjects;// ['MATH', 'AFM', 'CS'];
+const daysOfWeekAbbrev = staticData.daysOfWeekAbbrev;
+const daysOfWeekFullName = staticData.daysOfWeekFullName;
+const year = 2024;
+
+let apiObject = null;
 
 app.get("/api", (req, res) => {
-    axios.get('https://classes.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl?level=under&sess=1249&subject=MATH&cournum=%3F')
-    .then((webpage) => {
-        //console.log(web.data);
-        const $ = cheerio.load(webpage.data);
-        courseCodes = $('body > main > p > table > tbody > tr > td:nth-child(2):not(:last-child)')
-        .map((i, element) => $(element).text()).toArray();
-
-        courseNames = $('body > main > p > table > tbody > tr > td:nth-child(4)')
-        .map((i, element) => $(element).text()).toArray();
-
-        courseDetails = $('body > main > p > table > tbody > tr > td:nth-child(2):last-child > table > tbody > tr:not(:first-child) > td:nth-child(11)');
-        sections = courseDetails.map((idx, element) => {
-            txt = $(element).text();
-            // skip online or cancelled sections
-            if(txt == '' || !('0' <= txt.charAt(0) && txt.charAt(0) <= '9')) return;
-            // skip TST sections
-            if('0' <= txt.charAt(txt.length-1) && txt.charAt(txt.length-1) <= '9') return;
-            
-            startTime = new Date(2000, 0, 1, txt.slice(0,2), txt.slice(3,5));
-            endTime = new Date(2000, 0, 1, txt.slice(6,8), txt.slice(9,11));
-            if(startTime.getHours() < 8 || (startTime.getHours() == 8 || startTime.getMinutes() < 30)) {
-                startTime.setHours(startTime.getHours()+12);
-                endTime.setHours(endTime.getHours()+12);
-            }
-
-            daysOfWeek = [];
-            for(let j = 11, k = 0; k < daysOfWeekAbbre.length && j < txt.length; k++) {
-                if(txt.slice(j,j+daysOfWeekAbbre[k].length) == daysOfWeekAbbre[k]) {
-                    daysOfWeek.push(daysOfWeekAbbre[k]);
-                    j += daysOfWeekAbbre[k].length;
-                }
-            }
-            section = {
-                startTime: startTime,
-                endTime: endTime,
-                daysOfWeek: daysOfWeek
-            };
-            console.log(`${txt}   ${section.startTime} ${section.endTime} ${section.daysOfWeek}`);
-            return section;
-        }).toArray();
-        
-        // console.log(coursecodes);
-        // console.log(coursenames);
-        res.json({ courseCodes: courseCodes, courseNames: courseNames, sections: sections})
-    });
+    res.json(apiObject);
 });
 
 console.log("Hello world!");
 
-function parseClassTime(str) {
-    if(str == '') return [];
+function refreshAPI() {
+    const requests = subjects.map((subject) =>
+        axios.get(`https://classes.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl?level=under&sess=1249&subject=${subject}&cournum=%3F`));
+    Promise.all(requests).then((webpages) => {
+        const courses = [];
+        const sections = [];
+        for(let i = 0; i < webpages.length; i++) {
+            const $ = cheerio.load(webpages[i].data);
+
+            $('body > main > p > table > tbody > tr')
+            .filter((idx, element) => element.children.length == 8 || element.children.length == 2)
+            .each((idx, element) => {
+                
+                // course general info
+                if(element.children.length == 8) {
+                    // console.log($(element).text());
+                    courses.push({
+                        subject: $(element).children(':nth-child(1)').text().trim(),
+                        code: parseInt($(element).children(':nth-child(2)').text()),
+                        units: Number($(element).children(':nth-child(3)').text()),
+                        title: $(element).children(':nth-child(4)').text().trim(),
+                        sections: []
+                    });
+                } else {
+                    // course sections
+                    $(element).find('table > tbody > tr:not(:first-child)')
+                    .filter((idx, element) => element.children.length == 12)
+                    .each((idx, element) => {
+                        code = parseInt($(element).children(':first-child').text());
+                        // if(isNaN(code)) return;
+
+                        // console.log(`${$(element).text()}`);
+                        dateArr = $(element).children(':nth-child(11)').html().split('<br>');
+                        timeStr = dateArr[0];
+                        dateStr = dateArr[1] || '';
+                        console.log(`${timeStr}|${dateStr}\n`);
+                        // if(timeStr ==)
+                        
+                        let startTime = endTime = null;
+                        let daysOfWeek = [];
+                        if(timeStr != '') {
+                            startTime = new Date(1970, 0, 1, timeStr.slice(0,2), timeStr.slice(3,5));
+                            endTime = new Date(1970, 0, 1, timeStr.slice(6,8), timeStr.slice(9,11));
+                            if(startTime.getHours() < 8 || (startTime.getHours() == 8 && startTime.getMinutes() < 30)) {
+                                startTime.setHours(startTime.getHours()+12);
+                                endTime.setHours(endTime.getHours()+12);
+                            }
+
+                            // loop from monday to sunday, check for edge case 'T' and 'Th'
+                            for(let j = 11, k = 0; k < daysOfWeekAbbrev.length && j < timeStr.length; k++) {
+                                if(timeStr.slice(j,j+daysOfWeekAbbrev[k].length) == daysOfWeekAbbrev[k] && 
+                                    (daysOfWeekAbbrev[k] != 'T' || !(j+1 < timeStr.length && timeStr.charAt(j+1) == 'h'))) {
+                                    daysOfWeek.push(daysOfWeekAbbrev[k]);
+                                    j += daysOfWeekAbbrev[k].length;
+                                }
+                            }
+                        }
+                        let startDate = endDate = null;
+                        if(dateStr != '') {
+                            startDate = new Date(year, dateStr.slice(0,2)-1, dateStr.slice(3,5));
+                            endDate = new Date(year, dateStr.slice(6,8)-1, dateStr.slice(9,11));
+                        }
+
+                        if(!isNaN(code)) {
+                            sections.push({
+                                code: code,
+                                subject: courses[courses.length-1].subject,
+                                courseCode: courses[courses.length-1].code,
+                                component: $(element).children(':nth-child(2)').text().trim(),
+                                location: $(element).children(':nth-child(3)').text().trim().replace(/[\s]+/, ' '),
+                                times: [],
+                                enrollCap: parseInt($(element).children(':nth-child(7)').text()) || 0,
+                                enrollTotal: parseInt($(element).children(':nth-child(8)').text()) || 0
+                            });
+                            courses[courses.length-1].sections.push(code);
+                        }
+                        
+                        if(timeStr != '') {
+                            sections[sections.length-1].times.push({
+                                startTime: startTime,
+                                endTime: endTime,
+                                daysOfWeek: daysOfWeek,
+                                startDate: startDate,
+                                endDate: endDate
+                            });
+                        }
+                    });
+                }
+            });
+        }
+
+        const chartData1 = [];
+        const chartData2 = [];
+        const currDate = new Date(year, 8, 11);
+        let currTime = new Date(1970, 0, 1, 8, 30);
+        // 30 min timeslots from 8:30am to 10pm
+        for(let i = 0; i < 27; i++) {
+            nextHours = currTime.getHours();
+            nextMinutes = currTime.getMinutes()+30;
+            if(nextMinutes >= 60) {
+                nextHours++;
+                nextMinutes -= 60;
+            }
+            let nextTime = new Date(1970, 0, 1, nextHours, nextMinutes);
+            timeFrameName = `${currTime.getHours()}:${currTime.getMinutes().toString().padStart(2,'0')}`;
+            let timeFrame1 = {name: timeFrameName};
+            let timeFrame2 = {name: timeFrameName};
+            for(let j = 0; j < daysOfWeekAbbrev.length; j++) {
+                let enrollCapSum = 0;
+                let enrollTotalSum = 0;
+                sections.forEach((section) => {
+                    section.times.forEach((time) => {
+                        if(time.startTime == null || time.endTime == null) return;
+                        if(Math.max(time.startTime, currTime) < Math.min(time.endTime, nextTime) &&
+                            time.daysOfWeek.includes(daysOfWeekAbbrev[j]) &&
+                            ((time.startDate == null && time.endDate == null) || (time.startDate <= currDate && currDate <= time.endDate))) {
+                            enrollTotalSum += section.enrollTotal;
+                            enrollCapSum += section.enrollCap;
+                        }
+                    })
+                });
+                timeFrame1[daysOfWeekFullName[j]] = enrollTotalSum;
+                timeFrame2[daysOfWeekFullName[j]] = (enrollCapSum == 0 ? 0 : enrollTotalSum/enrollCapSum);
+            }
+            chartData1.push(timeFrame1);
+            chartData2.push(timeFrame2);
+            currTime = nextTime;
+        }
+        apiObject = {courses: courses, sections: sections, chartData1: chartData1, chartData2: chartData2};
+    });
 }
+refreshAPI();
+setInterval(refreshAPI, 600000);
